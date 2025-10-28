bb93e9df8add8288fa2f2fb8a04f228161f12e6c
gegxkss <ksyusha.lge@gmail.com>
2025-10-29 01:58:12 +0300

    task3

---
+++ task3/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		os.Exit(1)
+	}
+
+	numWorkers, err := strconv.Atoi(os.Args[1])
+
+	if err != nil || numWorkers < 2 {
+		os.Exit(1)
+	}
+
+	dataChannel := make(chan string)
+	// запуск воркеров
+	for i := 0; i < numWorkers; i++ {
+		go work(i, dataChannel)
+	}
+
+	//реализация постоянной записи данных, но с ограничением в 100 сообщений
+	for count := 1; count < 100; count++ {
+		message := fmt.Sprintf("Coобщение %d", count)
+		dataChannel <- message
+		count++
+	}
+	close(dataChannel)
+
+}
+
+func work(id int, dataChannel <-chan string) {
+	for data := range dataChannel {
+		fmt.Printf("Воркер номер %d: %s\n", id, data)
+	}
+}
